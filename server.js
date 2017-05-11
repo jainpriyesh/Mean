f0ca0e8e9a85ef8598928979ec89708c559f2fa0
jainpriyesh <priyshjn.31@gmail.com>
2017-05-11 08:04:22 +0000

    Session3

--- server.js
+++ server.js
@@ -4,8 +4,24 @@ var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
+app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(2000, function () {
+  console.log('Example app listening on port 2000!')
 });
+
+
+/*
+var Meow = mongoose.model('Meow',
+  {
+    text : String,
+    username : String 
+  });
+
+  app.get('/meows', function(req, res, next){
+      Meow.find({}, function(err, meows){
+        return res.json(meows);
+      });
+  });
+*/
 
